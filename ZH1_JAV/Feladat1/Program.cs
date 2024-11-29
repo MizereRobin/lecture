2848ac2aad661f6d624bab00ba5bc2af9fd50b2a
Mizere Robin Roland <mizererobin161@PETOFI-ASZOD.LAN>
2024-11-29 11:36:38 +0100

    1-2Feladatok

---
+++ ZH1_JAV/Feladat1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Feladat1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menüt? (I/N)");
+
+            bool menu = Console.ReadLine().ToLower() == "i" ? true : false;
+            string melyikMenu = "";
+            bool leves = false;
+            bool foetel = false;
+            bool savanyusag = false;
+            int desszert = -1;
+            bool kupon = false;
+
+            if (menu)
+            {
+                while (!(melyikMenu == "a" || melyikMenu == "b" || melyikMenu == "c" || melyikMenu == "vega")) //AltGr + W
+                {
+                    Console.WriteLine("Melyiket? (A, B, C, Vega)");
+                    melyikMenu = Console.ReadLine().ToLower();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Levest? (I/N)");
+                leves = Console.ReadLine().ToLower() == "i" ? true : false;
+                Console.WriteLine("Főetelt? (I/N)");
+                foetel = Console.ReadLine().ToLower() == "i" ? true : false;
+            }
+
+            Console.WriteLine("Savanyúságot? (I/N)");
+            savanyusag = Console.ReadLine().ToLower() == "i" ? true : false;
+
+            while (desszert < 0 || desszert > 5)
+            {
+                Console.WriteLine("Mennyi desszertet kér? (0-5)");
+                desszert = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Kupon?");
+            kupon = Console.ReadLine().ToLower() == "i" ? true : false;
+
+            int fizetendo = 0;
+
+            if (menu)
+            {
+                switch (melyikMenu)
+                {
+                    case "a":
+                        fizetendo += 1850;
+                        break;
+                    case "b":
+                        fizetendo += 2100;
+                        break;
+                    case "c":
+                        fizetendo += 1750;
+                        break;
+                    case "vega":
+                        fizetendo += 1950;
+                        break;
+                    default:
+                        Console.WriteLine("Valami nem jó");
+                        break;
+                }
+            }
+            else
+            {
+                fizetendo += leves ? 650 : 0;
+                fizetendo += foetel ? 1450 : 0;
+            }
+            fizetendo += savanyusag ? 600 : 0;
+            fizetendo += desszert * 750;
+
+            fizetendo = kupon ? Convert.ToInt32(fizetendo * 0.87) : 1;
+
+            Console.WriteLine("A végösszeg: "+fizetendo+"Ft lesz!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ZH1_JAV/Feladat2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] piros = { "kalap", "kendő", "cigi" };
+            string[] kék = { "ostor", "pisztoly"};
+            string[] zöld = { "nyereg", "táska", "pénzeszsák" };
+
+            bool BenneVan(string input, string[] szin)
+            {
+                foreach (var item in szin)
+                {
+                    if (item == input)
+                    {
+                        return true;
+                    }
+                    
+                }
+                return false;
+            }
+
+            int akasztok = 0; //használatban lévő akasztófülek
+            int pontok = 0;   //megszerzett pontok
+            Random rnd = new Random();
+
+            while (!(akasztok >= 10 || pontok >= 20))
+            {
+                Console.WriteLine("Mit tesz rá a csacsira?");
+                string input = Console.ReadLine();
+                double rand = rnd.NextDouble();
+                //if (input == "kalap" || input == "kendő" || input == "cigi")
+                if (BenneVan(input, piros))
+                {
+                    if (rand < 0.05)
+                    {
+                        Console.WriteLine("A csacsi lerúgta magáról a ráakasztott tárgyakat!");
+                        akasztok = 0;
+                        pontok = 0;
+                        break;
+                    }
+                    else
+                    {
+                        akasztok++;
+                        pontok++;
+                    }
+                }
+                //else if (input == "ostor" || input == "pisztoly")
+                else if (BenneVan(input, kék))
+                {
+                    if (rand < 0.1)
+                    {
+                        Console.WriteLine("A csacsi lerúgta magáról a ráakasztott tárgyakat!");
+                        akasztok = 0;
+                        pontok = 0;
+                        break;
+                    }
+                    else
+                    {
+                        akasztok++;
+                        pontok += 2;
+                    }
+                }
+                //else if (input == "nyereg" || input == "táska" || input == "pénzeszsák")
+                else if(BenneVan(input, zöld))
+                {
+                    if (rand < 0.15)
+                    {
+                        Console.WriteLine("A csacsi lerúgta magáról a ráakasztott tárgyakat!");
+                        akasztok = 0;
+                        pontok = 0;
+                        break;
+                    }
+                    else
+                    {
+                        akasztok++;
+                        pontok += 3;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nincs ilyen tárgy!");
+                }
+
+                Console.WriteLine($"{10-akasztok} van üresen, {pontok} pont van jelenleg");
+
+            }//while vége
+
+
+            if (akasztok == 0)
+            {
+                Console.WriteLine("Sajnos nem nyert! Próbálja újra!");
+            }
+            else if (akasztok==10)
+            {
+                Console.WriteLine("Gratulálok! Minden akasztó megtelt!");
+            }
+            if (pontok>=20)
+            {
+                Console.WriteLine("Gratulálok! Megszerezted a szükséges pontokat!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
