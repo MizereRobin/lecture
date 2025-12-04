49d4eeb746e4367a8e2033774fe248bd4e129511
MizereRobin <mizererobin@gmail.com>
2025-12-04 08:51:12 +0100

    Projekt_masolva

---
+++ Helsinki/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Helsinki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBeolvas = new System.Windows.Forms.Button();
+            this.button2Feladat = new System.Windows.Forms.Button();
+            this.button3Feladat = new System.Windows.Forms.Button();
+            this.button5Feladat = new System.Windows.Forms.Button();
+            this.button7Feladat = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonBeolvas
+            // 
+            this.buttonBeolvas.Location = new System.Drawing.Point(13, 13);
+            this.buttonBeolvas.Name = "buttonBeolvas";
+            this.buttonBeolvas.Size = new System.Drawing.Size(75, 23);
+            this.buttonBeolvas.TabIndex = 0;
+            this.buttonBeolvas.Text = "Beolvas";
+            this.buttonBeolvas.UseVisualStyleBackColor = true;
+            this.buttonBeolvas.Click += new System.EventHandler(this.buttonBeolvas_Click);
+            // 
+            // button2Feladat
+            // 
+            this.button2Feladat.Location = new System.Drawing.Point(13, 80);
+            this.button2Feladat.Name = "button2Feladat";
+            this.button2Feladat.Size = new System.Drawing.Size(75, 23);
+            this.button2Feladat.TabIndex = 1;
+            this.button2Feladat.Text = "2. Feladat";
+            this.button2Feladat.UseVisualStyleBackColor = true;
+            this.button2Feladat.Click += new System.EventHandler(this.button2Feladat_Click);
+            // 
+            // button3Feladat
+            // 
+            this.button3Feladat.Location = new System.Drawing.Point(13, 110);
+            this.button3Feladat.Name = "button3Feladat";
+            this.button3Feladat.Size = new System.Drawing.Size(75, 23);
+            this.button3Feladat.TabIndex = 2;
+            this.button3Feladat.Text = "3. Feladat";
+            this.button3Feladat.UseVisualStyleBackColor = true;
+            this.button3Feladat.Click += new System.EventHandler(this.button3Feladat_Click);
+            // 
+            // button5Feladat
+            // 
+            this.button5Feladat.Location = new System.Drawing.Point(13, 140);
+            this.button5Feladat.Name = "button5Feladat";
+            this.button5Feladat.Size = new System.Drawing.Size(75, 23);
+            this.button5Feladat.TabIndex = 3;
+            this.button5Feladat.Text = "5. Feladat";
+            this.button5Feladat.UseVisualStyleBackColor = true;
+            // 
+            // button7Feladat
+            // 
+            this.button7Feladat.Location = new System.Drawing.Point(13, 170);
+            this.button7Feladat.Name = "button7Feladat";
+            this.button7Feladat.Size = new System.Drawing.Size(75, 23);
+            this.button7Feladat.TabIndex = 4;
+            this.button7Feladat.Text = "7. Feladat";
+            this.button7Feladat.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(94, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(694, 425);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 456);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button7Feladat);
+            this.Controls.Add(this.button5Feladat);
+            this.Controls.Add(this.button3Feladat);
+            this.Controls.Add(this.button2Feladat);
+            this.Controls.Add(this.buttonBeolvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBeolvas;
+        private System.Windows.Forms.Button button2Feladat;
+        private System.Windows.Forms.Button button3Feladat;
+        private System.Windows.Forms.Button button5Feladat;
+        private System.Windows.Forms.Button button7Feladat;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Helsinki/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Helsinki
+{
+
+    public partial class Form1 : Form
+    {
+        private static List<Versenyzo> Versenyzok = new List<Versenyzo>();
+        private static List<Versenyzo> Dontosok = new List<Versenyzo>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //ide
+
+        }
+
+        private void buttonBeolvas_Click(object sender, EventArgs e)
+        {
+            #region Beolvasas
+            StreamReader sr = null;
+            string filepath = "rovidprogram.csv";
+            #region Rövidprogram
+            try
+            {
+
+                sr = new StreamReader(filepath);
+                sr.ReadLine(); // Fejléc miatt
+                while (!sr.EndOfStream)
+                {
+                    try
+                    {
+                        Versenyzok.Add(new Versenyzo(sr.ReadLine().Split(';')));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Hiba a versenyző hozzáadása során.");
+                    }
+                }
+                MessageBox.Show($"Sikeres olvasás: {filepath}");
+
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show($"Nincs ilyen file: {filepath}");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+            #endregion
+
+            filepath = "donto.csv";
+            #region Döntősök
+            try
+            {
+
+                sr = new StreamReader(filepath);
+                sr.ReadLine(); // Fejléc miatt
+                while (!sr.EndOfStream)
+                {
+                    try
+                    {
+                        Dontosok.Add(new Versenyzo(sr.ReadLine().Split(';')));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Hiba a versenyző hozzáadása során.");
+                    }
+                }
+                MessageBox.Show($"Sikeres olvasás: {filepath}");
+
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show($"Nincs ilyen file: {filepath}");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+            #endregion
+            #endregion
+        }
+
+        private void button2Feladat_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += 
+                $"2. Feladat\n\t" +
+                    $"A rövidprogramban {Versenyzok.Count} induló volt."; 
+        }
+
+        private void button3Feladat_Click(object sender, EventArgs e)
+        {
+            bool vanilyen = false ;
+            foreach (var item in Dontosok)
+            {
+                if (item.Orszag=="HUN")
+                    { vanilyen = true; }
+            }
+            if (vanilyen)
+            {
+                richTextBox1.Text +=
+                    "$\"3. Feladat\n\t" +
+                    "$\"A magyar versenyző bejutott a kűrbe.";
+            }
+            else
+            {
+                richTextBox1.Text +=
+                    "$\"3. Feladat\n\t" +
+                    "$\"A magyar versenyző nem jutott be a kűrbe.";
+            }
+
+        }
+
+        private double ÖsszPontszám()
+        {
+            return 0d;
+        }
+    }
+    class Versenyzo
+    {
+        #region Mezők
+        private string nev;
+        private string orszag;
+        private double technikai;
+        private double komponens;
+        private int levonas;
+        #endregion
+
+        #region Property-k
+        public string Nev { get { return nev; } set { nev = value; } } // Hagyományosan kiírt
+        public string Orszag { get => orszag; set => orszag = value; } // LAMBDA kifejezéssel
+        public double Technikai { get => technikai; set => technikai = value; }
+        public double Komponens { get => komponens; set => komponens = value; }
+        public int Levonas { get => levonas; set => levonas = value; }
+        #endregion
+
+        #region Konstruktor
+        //írd be: ctor üss két TAb-ot
+        public Versenyzo(string[] adatok)
+        {
+            Nev = adatok[0];
+            Orszag = adatok[1];
+            Technikai = double.Parse(adatok[2].Replace('.', ','));
+            Komponens = double.Parse(adatok[3].Replace('.', ','));
+            Levonas = int.Parse(adatok[4]);
+        }
+
+        #endregion
+    }
+}
